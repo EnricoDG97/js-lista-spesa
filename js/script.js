67b258483f0c7cdcafc3e3c127124bd9381211ea
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-01 20:10:38 +0100

    doc: created html js file and folders, added track and pseudcode

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Consegna:
+Data una lista della spesa (memorizzata in un array), 
+stampare sulla pagina (anche brutalmente, basta che si vedano) 
+gli elementi della lista individualmente con un ciclo while.
+
+
+ pseudcode:
+-creo un array di item da comprare, la mia lista della spesa
+-stampo in pagina ciascun elemento della lista usando ciclo while
+ */
